8ec3057ff0c2e6237f79c2609880f99747adcfcb
ehautoslash <eric@autoslash.com>
2026-04-08 13:02:45 -0600

    Create avis-autofill.meta.js

--- avissupport/avis-autofill.meta.js
+++ avissupport/avis-autofill.meta.js
@@ -1,1 +1,13 @@
-
+// ==UserScript==
+// @name         Avis Autofill Script
+// @namespace    https://github.com/ehautoslash/AvisSupportAutofill
+// @version      1.0.0
+// @description  Autofill Avis customer support form at avis.us.abgcustomerservice.com/createTicket
+// @author       Eric House
+// @match        https://avis.us.abgcustomerservice.com/createTicket
+// @grant        GM_setClipboard
+// @grant        GM_addStyle
+// @run-at       document-idle
+// @updateURL    https://raw.githubusercontent.com/ehautoslash/AvisSupportAutofill/main/avissupport/avis-autofill.meta.js
+// @downloadURL  https://raw.githubusercontent.com/ehautoslash/AvisSupportAutofill/main/avissupport/avis-autofill.user.js
+// ==/UserScript==
